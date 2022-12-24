c6820bd693de022637bde4221347adde8b8d695d
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2022-12-24 12:46:52 +0300

    zerotest

--- src/test/java/day05_maven/C01_MavenIlkTest.java
+++ src/test/java/day05_maven/C01_MavenIlkTest.java
@@ -8,23 +8,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.Key;
 import java.time.Duration;
+import java.util.List;
 
 public class C01_MavenIlkTest {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-        //amazon.com a gidelim
+        //1- https://www.amazon.com/ sayfasina gidelim
         driver.get("https://www.amazon.com/");
-        //arama kutusunu locate edelim
-        WebElement aramaKutusu= driver.findElement(By.xpath("twotabsearchtextbox"));
-        // "SAmsung headphones ile arama yapalım
-
+        //2- arama kutusunu locate edelim
+        WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
+        //3- “Samsung headphones” ile arama yapalim
         aramaKutusu.sendKeys("Samsung headphones" + Keys.ENTER);
+        //4- Bulunan sonuc sayisini yazdiralim
+        WebElement sonucYaziElementi= driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        System.out.println( sonucYaziElementi.getText());
+        //5-cıkan ilk ürünü yazdır.
+        driver.findElement(By.xpath("(//span[@class=\"a-size-medium a-color-base a-text-normal\"])[1]")).click();
+        //6- sayfadaki tüm başlıkları yazdıralım
+
+        driver.navigate().back();
+
+        List<WebElement> basliklarWebElementListesi=driver.findElements(By.xpath("//a[@class ='a-link-normal s-navigation-item']"));
+
+        for(WebElement each: basliklarWebElementListesi){
+            System.out.println(each.getText());
+        }
+
+        driver.close();
+
+
+
 
 
     }
